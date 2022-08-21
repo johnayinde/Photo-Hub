d58650025278c15f260aafebbc8a2161f2518854
Ayinde John <lolaayinde@gmail.com>
2022-08-21 10:31:31 +0100

    getting of a single category page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,43 +1,60 @@
 import {
   Controller,
   Get,
+  Param,
   Render,
   Req,
   Res,
   UseFilters,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
-import { AuthExceptionFilter } from './common/filters/auth-exceptions.filter';
 import { GalleriesService } from './galleries/galleries.service';
 import { UsersService } from 'src/users/users.service';
 import { SignInFilter } from './common/filters/signin-exceptions.filter';
 import { AuthenticatedGuard } from './common/guards/authenticated.guard';
+import { CategoriesService } from './categories/categories.service';
 
+@UseFilters(SignInFilter)
 @Controller()
-@UseFilters(AuthExceptionFilter)
 export class AppController {
   constructor(
     private readonly galleryService: GalleriesService,
+    private readonly categporyService: CategoriesService,
     private readonly userService: UsersService,
   ) {}
+
+  @Get('/category/:name')
+  @Render('home')
+  async categoryHomePage(@Req() req, @Param('name') name: string) {
+    const categories = await this.categporyService.findAll();
+
+    const categoryPhotos = await this.categporyService.findOne(name);
+    console.log({ categoryPhotos });
+
+    const photos = categoryPhotos.photos;
+    console.log({ photos });
+    return { photos, categories: categories };
+  }
 
   @Get('/')
   @Render('home')
   async homePage(@Req() req) {
+    const size = 3;
     const allGalleries = await this.galleryService.findAll();
-    return { galleries: allGalleries };
+    const categories = await this.categporyService.findAll();
+
+    return {
+      galleries: allGalleries,
+      categories: categories,
+      moreCat: categories.slice(size),
+    };
   }
 
-  @UseFilters(SignInFilter)
+  @Get('/dashboard')
+  @Render('dashboard')
   @UseGuards(AuthenticatedGuard)
-  @Get('/profile')
-  @Render('profile')
-  async profilePage(@Req() req, @Res() res) {
+  async dashboardPage(@Req() req, @Res() res) {
     const allGalleries = await this.userService.getUserGalleries(req.user.id);
-    console.log(allGalleries);
-
     return { galleries: allGalleries };
   }
 }
